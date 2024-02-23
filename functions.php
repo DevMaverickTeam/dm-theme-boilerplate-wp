64d93ce32df0ea0b310a2de905d8dd70306de121
George-Paul Cretu <george.cretu@gmail.com>
2024-02-23 14:09:45 +0200

    Commented out some elements

--- functions.php
+++ functions.php
@@ -201,9 +201,9 @@ if ( ! class_exists( 'wp_bootstrap_navwalker' )) {
 }
 
 
-require_once('config/shortcodes.php');
 require_once('config/assets.php');
 require_once('config/acf-style.php');
-require_once('config/acf-option-pages.php');
-require_once('config/post-types.php');
-require_once('config/custom-taxonomies.php');
+// require_once('config/shortcodes.php');
+// require_once('config/acf-option-pages.php');
+// require_once('config/post-types.php');
+// require_once('config/custom-taxonomies.php');
